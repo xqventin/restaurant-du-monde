673d15c66c864e8104c17b21c5b5c10715d79c02
Quentin Honerchick <honerchick.quentin@icloud.com>
2018-10-01 23:42:18 +0200

    Ajout des contrôleurs

--- Restaurant du Monde/Controlleur/MenuController.swift
+++ Restaurant du Monde/Controlleur/MenuController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MenuController.swift
 //  Restaurant du Monde
 //
 //  Created by Quentin Honerchick on 01/10/2018.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MenuController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+
+   
 
 }
-
